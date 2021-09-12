131f5b8ea6e174538871538a7b65263bbc66ac05
Donghan Kim <troth1532@naver.com>
2021-09-12 23:29:21 +0900

    exception handling

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,15 +18,19 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HelloA");
+        try{
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
 
-        em.persist(member);
+            em.persist(member);
 
-        tx.commit();
-
-        em.close();
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
 
         emf.close();
 
