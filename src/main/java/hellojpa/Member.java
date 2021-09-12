1998d6f7d9003efab8d49fb7d7317385e08572ea
Donghan Kim <troth1532@naver.com>
2021-09-12 23:26:06 +0900

    first commit

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,30 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    private Long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    //Getter, Setter ...
+}
