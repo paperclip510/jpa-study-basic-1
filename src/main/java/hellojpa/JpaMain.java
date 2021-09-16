38936bdcc18d606333c51d298902a5c5c438b570
Donghan Kim <troth1532@naver.com>
2021-09-17 07:52:46 +0900

    persistence test

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -22,33 +22,13 @@ public class JpaMain {
         tx.begin();
 
         try{
-            // 등록
-            //  Member member = new Member();
-            //  member.setId(2L);
-            //  member.setName("HelloB");
-            // em.persist(member);
+            //비영속 상태.
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HEEELLO");
 
-
-            // 수정 // persist 하지 않아도 update 쿼리를 실행한다.
-            // 업데이트 여부(변경감지)를 확인하고 tx.commit() 전 자동 업데이트 진행.
-            // Member findMember =  em.find(Member.class, 1L);
-            // findMember.setName("HelloJPA");
-
-
-            //삭제
-            //em.remove(findMember);
-
-            // JPQL
-            List<Member> memberList
-                    = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(2)
-                    .getResultList();
-
-            for(Member item : memberList){
-                System.out.println(item.getId() + ", " + item.getName());
-            }
-
+            //영속 상태.
+            em.persist(member);
 
             tx.commit();
         }catch (Exception e){
